4f11be0d6bda92b33a5124901f1d3b5cd032a3b4
Ewelai <palevo.ewelai@gmail.com>
2020-03-26 22:18:41 +0300

    fix: properties in tests

--- src/app/generic-view/diagnose-text-view/diagnose-text-view.component.spec.ts
+++ src/app/generic-view/diagnose-text-view/diagnose-text-view.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DiagnoseTextViewComponent } from './diagnose-text-view.component';
 
@@ -8,7 +10,9 @@ describe('DiagnoseTextViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DiagnoseTextViewComponent ]
+      declarations: [ DiagnoseTextViewComponent ],
+      imports: [ HttpClientModule ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
--- src/app/shared-components/api-data/api-data.service.spec.ts
+++ src/app/shared-components/api-data/api-data.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { ApiDataService } from './api-data.service';
 
 describe('ApiDataService', () => {
+  let httpClient:HttpClient;
+  let httpTestingController:HttpTestingController;
   let service: ApiDataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ApiDataService);
   });
 
